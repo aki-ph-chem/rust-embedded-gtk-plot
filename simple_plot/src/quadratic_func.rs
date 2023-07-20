cb86bf6d577d2867db640b46b0eb1f8ba2b2d746
aki <aki@mymail.com>
2023-07-20 19:39:48 +0900

    gtk::Windowの中に二次関数のplotを埋め込む

---
+++ simple_plot/src/quadratic_func.rs
@@ -0,0 +1,66 @@
+use std::error;
+use gtk;
+use gtk::prelude::*;
+use plotters::prelude::*;
+use plotters_cairo::CairoBackend;
+
+
+// gtk windowの中にplotを埋め込む
+fn build_ui(app: &gtk::Application) {
+    let ui = include_str!("ui/q_func.ui");
+    let builder = gtk::Builder::from_string(ui);
+    let window: gtk::Window = builder.object("window").expect("Error: window");
+    window.set_application(Some(app));
+
+    let drawing_area: gtk::DrawingArea = builder.object("draw")
+        .expect("Error: draw");
+
+    drawing_area.connect_draw(move |widget, cr| {
+        let width = widget.allocated_width();
+        let height = widget.allocated_height();
+
+        let backend = CairoBackend::new(cr, (width as u32, height as u32))
+            .unwrap();
+        let root = backend.into_drawing_area();
+        root.fill(&WHITE).unwrap();
+        let mut chart = ChartBuilder::on(&root)
+            .caption("y=x^2", ("sans-serif", 50).into_font())
+            .margin(5)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32).unwrap();
+
+        chart.configure_mesh().draw().unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                    (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+                    &RED,
+                    )).unwrap()
+            .label("y = x^2")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+        chart
+            .configure_series_labels()
+            .background_style(&WHITE.mix(0.8))
+            .border_style(&BLACK)
+            .draw()
+            .unwrap();
+
+        root.present().unwrap();
+
+        Inhibit(false)
+    });
+
+    window.show_all();
+} 
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let application = gtk::Application::new(
+        Some("gtk.plot"), Default::default());
+
+    application.connect_activate(build_ui);
+    application.run();
+
+    Ok(())
+}
