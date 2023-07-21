cc15aaf77731824edb5be0f230b5e4c1ab5214d3
aki <aki@mymail.com>
2023-07-21 16:22:50 +0900

    plotをする関数のエラー処理を改善した

--- simple_plot/src/quadratic_func.rs
+++ simple_plot/src/quadratic_func.rs
@@ -5,15 +5,16 @@ use plotters::prelude::*;
 use plotters_cairo::CairoBackend;
 
 // backendに対してplot
-fn plot_q_func(backend: CairoBackend) {
+fn plot_q_func(backend: CairoBackend) -> Result<(), Box<dyn error::Error>> {
         let root = backend.into_drawing_area();
-        root.fill(&WHITE).unwrap();
+        root.fill(&WHITE)?;
+        
         let mut chart = ChartBuilder::on(&root)
             .caption("y=x^2", ("sans-serif", 50).into_font())
             .margin(5)
             .x_label_area_size(30)
             .y_label_area_size(30)
-            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32).unwrap();
+            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
 
         chart.configure_mesh().draw().unwrap();
 
@@ -29,10 +30,10 @@ fn plot_q_func(backend: CairoBackend) {
             .configure_series_labels()
             .background_style(&WHITE.mix(0.8))
             .border_style(&BLACK)
-            .draw()
-            .unwrap();
+            .draw()?;
 
-        root.present().unwrap();
+        root.present()?;
+        Ok(())
 }
 
 // gtk windowの中にplotを埋め込む
@@ -52,7 +53,7 @@ fn build_ui(app: &gtk::Application) {
         let backend = CairoBackend::new(cr, (width as u32, height as u32))
             .unwrap();
 
-        plot_q_func(backend);
+        plot_q_func(backend).unwrap();
         Inhibit(false)
     });
 
